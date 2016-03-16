71f5ec16bdda7917d2d33e3b1ea0decb8d9a5971
unknown <bianca.agoncalves@CAS400441W1021.senacsp.edu.br>
2016-03-16 20:55:12 -0300

    Exemplo 1

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Conding Standards";
+
+$nome = "Bianca";
+
+$nota = 1 + 2;
+
+// $nome_sobrenome = "Bianca Alencar" ."Goncalves";
+
+$nome_sobrenome = "Bianca Alencar" ." Goncalves"; //ponto serve para concatenar
+
+echo '<br><br> Nome: '.$nome_sobrenome;
+
+
+?> 
